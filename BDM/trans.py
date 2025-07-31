56044155ccf2a214906e0e712b209b20ab1245a3
psanayuki <87215011+psanayuki@users.noreply.github.com>
2025-07-31 15:06:57 +0800

    Add files via upload

---
+++ BDM/trans.py
@@ -0,0 +1,150 @@
+import torch.nn as nn
+import torch as th
+from step_sample import create_named_schedule_sampler
+import numpy as np
+import math
+import torch
+import torch.nn.functional as F
+
+class B_LayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-12):
+        """Construct a layernorm module in the TF style (epsilon inside the square root).
+        """
+        super(B_LayerNorm, self).__init__()
+        self.weight = nn.Parameter(torch.ones(hidden_size))
+        self.bias = nn.Parameter(torch.zeros(hidden_size))
+        self.variance_epsilon = eps
+
+    def forward(self, x):
+        u = x.mean(-1, keepdim=True)
+        s = (x - u).pow(2).mean(-1, keepdim=True)
+        x = (x - u) / torch.sqrt(s + self.variance_epsilon)
+        return self.weight * x + self.bias
+    
+class B_SublayerConnection(nn.Module):
+    """
+    A residual connection followed by a layer norm.
+    Note for code simplicity the norm is first as opposed to last.
+    """
+
+    def __init__(self, hidden_size, dropout):
+        super(B_SublayerConnection, self).__init__()
+        self.norm = B_LayerNorm(hidden_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, sublayer):
+        "Apply residual connection to any sublayer with the same size."
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+class B_PositionwiseFeedForward(nn.Module):
+    "Implements FFN equation."
+
+    def __init__(self, hidden_size, dropout=0.1):
+        super(B_PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(hidden_size, hidden_size*4)
+        self.w_2 = nn.Linear(hidden_size*4, hidden_size)
+        self.dropout = nn.Dropout(dropout)
+        
+        self.initializer_range = 0.02
+        self.apply(self._init_weights)
+
+    
+    # recbole SASRec 
+    def _init_weights(self, module):
+        """Initialize the weights"""
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            module.weight.data.normal_(mean=0.0, std=self.initializer_range)
+        elif isinstance(module, B_LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+            
+    #     self.init_weights()
+
+    # def init_weights(self):
+    #     nn.init.xavier_normal_(self.w_1.weight)
+    #     nn.init.xavier_normal_(self.w_2.weight)
+
+    def forward(self, hidden):
+        hidden = self.w_1(hidden)
+        activation = 0.5 * hidden * (1 + torch.tanh(math.sqrt(2 / math.pi) * (hidden + 0.044715 * torch.pow(hidden, 3))))
+        return self.w_2(self.dropout(activation))
+
+
+class B_MultiHeadedAttention(nn.Module):
+    def __init__(self, heads, hidden_size, dropout):
+        super().__init__()
+        assert hidden_size % heads == 0
+        self.size_head = hidden_size // heads
+        self.num_heads = heads
+        self.linear_layers = nn.ModuleList([nn.Linear(hidden_size, hidden_size) for _ in range(3)])
+        self.w_layer = nn.Linear(hidden_size, hidden_size)
+        self.dropout = nn.Dropout(p=dropout)
+        
+        self.initializer_range = 0.02
+        self.apply(self._init_weights)
+
+    
+    # recbole SASRec 
+    def _init_weights(self, module):
+        """Initialize the weights"""
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            module.weight.data.normal_(mean=0.0, std=self.initializer_range)
+        elif isinstance(module, B_LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+    #     self.init_weights()
+
+    # def init_weights(self):
+    #     nn.init.xavier_normal_(self.w_layer.weight)
+
+    def forward(self, q, k, v, mask=None):
+        batch_size = q.shape[0]
+        q, k, v = [l(x).view(batch_size, -1, self.num_heads, self.size_head).transpose(1, 2) for l, x in zip(self.linear_layers, (q, k, v))]
+        corr = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(q.size(-1))
+        
+        if mask is not None:
+            mask = mask.unsqueeze(1).repeat([1, corr.shape[1], 1]).unsqueeze(-1).repeat([1,1,1,corr.shape[-1]])
+            corr = corr.masked_fill(mask == 0, -1e9)
+        prob_attn = F.softmax(corr, dim=-1)
+        if self.dropout is not None:
+            prob_attn = self.dropout(prob_attn)
+        hidden = torch.matmul(prob_attn, v)
+        hidden = self.w_layer(hidden.transpose(1, 2).contiguous().view(batch_size, -1, self.num_heads * self.size_head))
+        return hidden
+
+
+class B_TransformerBlock(nn.Module):
+    def __init__(self, hidden_size, attn_heads, dropout):
+        super(B_TransformerBlock, self).__init__()
+        self.attention = B_MultiHeadedAttention(heads=attn_heads, hidden_size=hidden_size, dropout=dropout)
+        self.feed_forward = B_PositionwiseFeedForward(hidden_size=hidden_size, dropout=dropout)
+        self.input_sublayer = B_SublayerConnection(hidden_size=hidden_size, dropout=dropout)
+        self.output_sublayer = B_SublayerConnection(hidden_size=hidden_size, dropout=dropout)
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, hidden, mask):
+        hidden = self.input_sublayer(hidden, lambda _hidden: self.attention.forward(_hidden, _hidden, _hidden, mask=mask))
+        hidden = self.output_sublayer(hidden, self.feed_forward)
+        return self.dropout(hidden)
+
+
+class B_Transformer_rep(nn.Module):
+    def __init__(self, args):
+        super(B_Transformer_rep, self).__init__()
+        self.hidden_size = args.hidden_size
+        self.heads = args.n_heads # 4
+        self.dropout = args.dropout
+        self.n_blocks = args.n_layers # args.num_blocks
+        self.transformer_blocks = nn.ModuleList(
+            [B_TransformerBlock(self.hidden_size, self.heads, self.dropout) for _ in range(self.n_blocks)])
+
+    def forward(self, hidden, mask):
+        for transformer in self.transformer_blocks:
+            hidden = transformer.forward(hidden, mask)
+        return hidden
